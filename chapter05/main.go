63a541492348db4ef834de526c6a05a343f7c175
Sammy <xiangmin.li@qq.com>
2016-10-05 17:29:21 +0800

    add listing 5.66

--- chapter05/main.go
+++ chapter05/main.go
@@ -1,71 +1,25 @@
-// Listing 5.60
-// Sample program to show what happens when the outer and inner
-// type implement the same interface.
+// Listing 5.64
+// Sample program to show how the program can't access an
+// unexported identifier from another package.
 package main
 
 import (
 	"fmt"
+
+	"sammietocat/2016.go.in.action/chapter05/counters"
 )
-
-// notifier is an interface that defined notification
-// type behavior.
-type notifier interface {
-	notify()
-}
-
-// user defines a user in the program.
-type user struct {
-	name  string
-	email string
-}
-
-// notify implements a method that can be called via
-// a value of type user.
-func (u *user) notify() {
-	fmt.Printf("Sending user email to %s<%s>\n",
-		u.name,
-		u.email)
-}
-
-// admin represents an admin user with privileges.
-type admin struct {
-	user
-	level string
-}
-
-// notify implements a method that can be called via
-// a value of type Admin.
-func (a *admin) notify() {
-	fmt.Printf("Sending admin email to %s<%s>\n",
-		a.name,
-		a.email)
-}
 
 // main is the entry point for the application.
 func main() {
-	// Create an admin user.
-	ad := admin{
-		user: user{
-			name:  "john smith",
-			email: "john@yahoo.com",
-		},
-		level: "super",
-	}
+	// Create a variable of the unexported type and initialize
+	// the value to 10.
+	counter := counters.alertCounter(10)
 
-	// Send the admin user a notification.
-	// The embedded inner type's implementation of the
-	// interface is NOT "promoted" to the outer type.
-	sendNotification(&ad)
+	/*
+		./main.go:15: cannot refer to unexported name counters.alertCounter
+		./main.go:15: undefined: counters.alertCounter
+		exit status 2
+	*/
 
-	// We can access the inner type's method directly.
-	ad.user.notify()
-
-	// The inner type's method is NOT promoted.
-	ad.notify()
-}
-
-// sendNotification accepts values that implement the notifier
-// interface and sends notifications.
-func sendNotification(n notifier) {
-	n.notify()
+	fmt.Printf("Counter: %d\n", counter)
 }
