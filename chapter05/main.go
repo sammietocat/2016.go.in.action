eb5cdcd9d0daab57b7ca86dcdec31b02222a30e7
Sammy <xiangmin.li@qq.com>
2016-10-05 16:36:37 +0800

    add listing 5.34

--- chapter05/main.go
+++ chapter05/main.go
@@ -1,44 +1,36 @@
 /**
-  Demo for method
+  Listing 5.34
+  Sample program to show how to write a simple version of curl using the io.Reader and io.Writer interface support
 */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
-/* Listing 5.11 */
-// Sample program to show how to declare methods and how the Go compiler supports them.
-
-// user defines a user in the program
-type user struct {
-	name  string
-	email string
+// init is called before main.
+func init() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./main <url>")
+		os.Exit(-1)
+	}
 }
 
-// notify implements a method with a value receiver
-func (u user) notify() {
-	fmt.Printf("Sending User Email To %s<%s>\n", u.name, u.email)
-}
-
-// changeEmail implements a method with a pointer receiver
-func (u *user) changeEmail(email string) {
-	u.email = email
-}
-
-// main is the entry point for the application.
+// main is the entry point for the application
 func main() {
-	// Values of type user can be used to call methods
-	// declared with a value receiver.
-	bill := user{"Bill", "bill@email.com"}
-	bill.notify()
-	// Pointers of type user can also be used to call methods
-	// declared with a value receiver.
-	lisa := &user{"Lisa", "lisa@email.com"}
-	lisa.notify()
-	// Values of type user can be used to call methods
-	// declared with a pointer receiver.
-	bill.changeEmail("bill@newdomain.com")
-	bill.notify() // Pointers of type user can be used to call methods
-	// declared with a pointer receiver.
-	lisa.changeEmail("lisa@comcast.com")
-	lisa.notify()
+	// Get a response from the web server
+	r, err := http.Get(os.Args[1])
+	if nil != err {
+		fmt.Println(err)
+		return
+	}
+
+	// Copies from the Body to Stdout
+	io.Copy(os.Stdout, r.Body)
+	if err := r.Body.Close(); err != nil {
+		fmt.Println(err)
+	}
 }
