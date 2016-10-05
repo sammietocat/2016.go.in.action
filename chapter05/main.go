d55c9a2982d0686690c97fc502da4da24e5f5827
Sammy <xiangmin.li@qq.com>
2016-10-05 17:02:43 +0800

    add listing 5.48

--- chapter05/main.go
+++ chapter05/main.go
@@ -1,25 +1,56 @@
-// Listing 5.46
-// Sample program to show how you cannot always get the address of a value
+// Listing 5.48
+// Sample program to show how polymorphic behavior with interfaces.
 package main
 
 import (
 	"fmt"
 )
 
-// duration is a type with a base type of int.
-type duration int
+// notifier is an interface that defined notification
+// type behavior.
+type notifier interface {
+	notify()
+}
 
-func (d *duration) pretty() string {
-	return fmt.Sprintf("Duration: %d", *d)
+// user defines a user in the program.
+type user struct {
+	name  string
+	email string
+}
+
+// notify implements the notifier interface with a pointer receiver.
+func (u *user) notify() {
+	fmt.Printf("Sending user email to %s<%s>\n",
+		u.name,
+		u.email)
+}
+
+// admin defines a admin in the program.
+type admin struct {
+	name  string
+	email string
+}
+
+// notify implements the notifier interface with a pointer receiver.
+func (a *admin) notify() {
+	fmt.Printf("Sending user email to %s<%s>\n",
+		a.name,
+		a.email)
 }
 
 // main is the entry point for the application.
 func main() {
-	duration(42).pretty()
+	// Create a user value and pass it to  snedNotification.
+	bill := user{"Bill", "bill@email.com"}
+	sendNotification(&bill)
 
-	/*
-		./main.go:18: cannot call pointer method on duration(42)
-		./main.go:18: cannot take the address of duration(42)
-		exit status 2
-	*/
+	// Create a admin value and pass it to  snedNotification.
+	lisa := user{"Lisa", "lisa@email.com"}
+	sendNotification(&lisa)
+}
+
+// sendNotification accepts values that implement the notifier
+// interface and sends notifications.
+func sendNotification(n notifier) {
+	n.notify()
 }
