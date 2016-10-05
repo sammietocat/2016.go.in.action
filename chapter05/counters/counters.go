d9277cb767e513f05018a429ebf09974fdf7a928
Sammy <xiangmin.li@qq.com>
2016-10-05 17:33:29 +0800

    add listing 5.68

--- chapter05/counters/counters.go
+++ chapter05/counters/counters.go
@@ -4,3 +4,8 @@ package counters
 // alertCounter is an unexported type that
 // contains an integer counter for alerts.
 type alertCounter int
+
+// New creates and returns values of the unexported type alertCounter
+func New(value int) alertCounter {
+	return alertCounter(value)
+}
--- chapter05/main.go
+++ chapter05/main.go
@@ -1,4 +1,4 @@
-// Listing 5.64
+// Listing 5.68
 // Sample program to show how the program can't access an
 // unexported identifier from another package.
 package main
@@ -13,7 +13,7 @@ import (
 func main() {
 	// Create a variable of the unexported type and initialize
 	// the value to 10.
-	counter := counters.alertCounter(10)
+	counter := counters.New(10)
 
 	/*
 		./main.go:15: cannot refer to unexported name counters.alertCounter
